63c4d0d328e42c86ac7f5c60319e9bec8556706b
MannRobertJ <mann.robertj@gmail.com>
2019-01-18 21:05:53 +0100

    cleaning

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -22,12 +22,4 @@ class ShoppingCart {
   }
 }
 
-const cart = new ShoppingCart();
-cart.addItem("item 1", 1, 15.5);
-cart.addItem("item 2", 2, 3);
-cart.addItem("item 3", 10, 2);
-cart.addItem("item 4", 0, 2);
-cart.addItem("item 5", 2, 0);
-
-console.log(cart.total())
 module.exports = ShoppingCart;
